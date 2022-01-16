781b6ee6a1ea544f23e20056ceee9e6747460ba2
StealthElf98 <sergej.dragojlovic@gmail.com>
2022-01-16 19:29:41 +0100

    Initial Commit

---
+++ Memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memories
+//
+//  Created by Sergej on 16.1.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Sergej on 16.1.22..
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
