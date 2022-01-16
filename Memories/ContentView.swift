2a005f89ad19558b42bc371cbc537f81b8235054
StealthElf98 <sergej.dragojlovic@gmail.com>
2022-01-17 00:28:29 +0100

    First day finished

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🚝", "🥲", "🤌", "🤡"]
+    var emojiCount = 3
+    
     var body: some View {
-        Text("Hello, Sergej, provera da li git radi!")
-            .padding()
+        VStack{
+            HStack{
+                ForEach(emojis[0...emojiCount], id: \.self, content: { emoji in
+                    CardView(content: emoji)
+                })
+            }
+            Button(action: {}, label: {
+                VStack{
+                    Text("Add")
+                    Text("Card")
+                }
+            })
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
+    }
+    
+}
+
+struct CardView : View {
+    var content : String
+    @State var isFaceUp : Bool = true
+    
+    var body: some View {
+        
+        let shape = RoundedRectangle(cornerRadius: 25)
+        
+        ZStack {
+            if isFaceUp {
+                shape.fill(.white)
+                shape.stroke(lineWidth: 6)
+            Text(content)
+                .font(.largeTitle)
+            }else {
+                shape.fill(.red)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+
     }
 }
