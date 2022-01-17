e594c49eab05389e39f00486fffe66f9dc76f3f8
StealthElf98 <sergej.dragojlovic@gmail.com>
2022-01-17 22:55:57 +0100

    Assignment 1 finished

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -9,28 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var emojis = ["🚝", "🥲", "🤌", "🤡"]
-    var emojiCount = 3
+    @State var emojis = ["✈️","🚝", "🚈", "🚘", "🚖", "🚊"]
+    @State var emojiCount = 5
     
     var body: some View {
         VStack{
-            HStack{
-                ForEach(emojis[0...emojiCount], id: \.self, content: { emoji in
-                    CardView(content: emoji)
-                })
-            }
-            Button(action: {}, label: {
-                VStack{
-                    Text("Add")
-                    Text("Card")
+            Text("Memorize!")
+                .font(.largeTitle)
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]){
+                    ForEach(emojis[0...emojiCount], id: \.self, content: { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    })
                 }
-            })
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                cars
+                Spacer()
+                persons
+                Spacer()
+                square
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
         
     }
     
+    var persons : some View {
+        Button {
+            emojiCount = Int.random(in: 1...5)
+            emojis = ["🤦🏼‍♀️", "👨🏻‍🦰", "👱🏼‍♀️", "🧍🏼‍♂️", "👱🏿‍♂️", "👨🏼‍🦳", "👽"]
+                .shuffled()
+        }label: {
+            VStack{
+                Image(systemName: "person")
+                Text("People")
+                    .font(.title2)
+            }
+        }
+        
+    }
+    
+    var square : some View {
+        Button {
+            emojiCount = Int.random(in: 1...5)
+            emojis = ["🔴", "🟩", "🌙", "🔶", "🔻", "🔺", "💠"]
+                .shuffled()
+        }label: {
+            VStack{
+                Image(systemName: "square")
+                Text("Shapes")
+                    .font(.title2)
+            }
+        }
+        
+    }
+    
+    var cars : some View {
+        Button(action: {
+            emojiCount = Int.random(in: 1...5)
+            emojis = ["✈️","🚝", "🚈", "🚘", "🚖", "🚊"]
+                .shuffled()
+        }, label: {
+            VStack{
+                Image(systemName: "car")
+                Text("Transport")
+                    .font(.title2)
+            }
+        })
+    }
 }
 
 struct CardView : View {
@@ -44,7 +96,7 @@ struct CardView : View {
         ZStack {
             if isFaceUp {
                 shape.fill(.white)
-                shape.stroke(lineWidth: 6)
+                shape.strokeBorder(lineWidth: 3)
             Text(content)
                 .font(.largeTitle)
             }else {
